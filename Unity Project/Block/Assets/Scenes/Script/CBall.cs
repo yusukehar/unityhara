f9b1a13d58a3edcfac5f00c0746902cba7a50021
DESKTOP-JDC47GA\y87ha <y87hara@gmail.com>
2017-04-23 21:52:45 +0900

    no message

---
+++ Unity Project/Block/Assets/Scenes/Script/CBall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CBall : MonoBehaviour {
+	public float INIT_DEGREE = 75f;
+	public float INIT_SPEED = 40f;
+
+	// Use this for initialization
+	void Start () {
+		shotBall ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void shotBall(){
+		Vector3 vel = Vector3.zero;
+		vel.x = INIT_SPEED * Mathf.Cos (INIT_DEGREE * Mathf.PI / 180f);
+		vel.y = INIT_SPEED * Mathf.Sin (INIT_DEGREE * Mathf.PI / 180f);
+		GetComponent<Rigidbody>().velocity = vel;
+	}
+}
---
+++ Unity Project/Block/Assets/Scenes/Script/CPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CPlayer : MonoBehaviour {
+	public float VEL = 40f;
+	public GameObject prefBall = null;
+   	GameObject insBall = null;
+
+
+	void Start () {
+		createHoldBall ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 vel = Vector3.zero;
+		vel.x = VEL * Input.GetAxisRaw ("Horizontal");
+		GetComponent<Rigidbody> ().velocity = vel;
+	}
+
+	void createHoldBall(){
+		Vector3 bpos = transform.position;
+		bpos.y += (GetComponent<Collider> ().bounds.size.y + prefBall.transform.localScale.y) / 2f;
+		insBall = (GameObject)Instantiate (prefBall, bpos, Quaternion.identity);
+		insBall.transform.parent = transform;
+	}
+}
